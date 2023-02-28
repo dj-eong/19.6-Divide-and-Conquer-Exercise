a9221cd8a1262a6690c3fe3f623a1caa4ceb7842
dj-eong <118857620+dj-eong@users.noreply.github.com>
2023-02-27 23:56:56 -0500

    Update find-rotated-index.js

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,30 +1,15 @@
-// function findRotatedIndex(arr, val) {
-//     let left = 0;
-//     let right = arr.length - 1;
-//     while (left <= right) {
+// Write a function called ***findRotatedIndex*** which accepts a rotated array of sorted numbers and an integer. 
+// The function should return the index of num in the array. If the value is not found, return -1.
 
-//         let middle = Math.floor((left + right) / 2);
+// **Constraints**:
+// Time Complexity: O(log N)
 
-//         if (arr[arr.length - 1] < val) {
-//             left = middle + 1;
-//         } else if (arr[arr.length - 1] > val) {
-//             right = middle;
-//         }
-
-
-
-//         if (arr[middle] < val) {
-
-//             left = middle + 1;
-//         } else if (arr[middle] > val) {
-
-//             right = middle - 1;
-//         } else {
-
-//             return middle;
-//         }
-//     }
-// }
+// Examples:
+// findRotatedIndex([3,4,1,2],4) // 1
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
+// findRotatedIndex([37,44,66,102,10,22],14) // -1
+// findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 
 function findRotatedIndex(arr, val) {
     const firstIndex = findFirstIndex(arr);
